19ebc96f2d36b03d9dd9b2afd64c51424acf1120
IrrigationGuy <109033294+IrrigationGuy@users.noreply.github.com>
2022-07-17 22:21:53 +0300

    Update soil_moisture_sensor_calibrating.ino

--- Code/soil_moisture_sensor_calibrating.ino
+++ Code/soil_moisture_sensor_calibrating.ino
@@ -1,7 +1,7 @@
 //the code shows the bit readings from the sensor every 1 milli-second.
 //first watch the serial monitor while the sensor is completly dry, wait a minute and decide on an average value - that's the dry value
 //then put the sensor in a glass of water (not fully submerged), that's the 100% wet value, decide on an average value like before.
-//right these values own, these would be the "wet##" and "dry##" consts for this specific sensor in the project's code. 
+//right these values down, these would be the "wet##" and "dry##" consts for this specific sensor in the project's code. 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
